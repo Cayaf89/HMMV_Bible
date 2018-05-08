c2f333169c973c88e1b0075b46d1520c000514c2
Cayaf89 <corentin.fayac@orange.fr>
2018-05-08 12:34:56 +0200

    image quicklink

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -36,49 +36,138 @@ class DashboardController extends Controller
             array(
                 'label' => "Artéfacts",
                 'slug'  => "artefact",
-                'fa'    => "quidditch",
                 'nb'    => count($this->getDoctrine()->getRepository(Artefact::class)->findAll()),
             ),
             array(
                 'label' => "Batîments",
                 'slug'  => "batiment",
-                'fa'    => "home",
                 'nb'    => count($this->getDoctrine()->getRepository(Batiment::class)->findAll()),
             ),
             array(
                 'label' => "Capacités",
                 'slug'  => "capacite",
-                'fa'    => "band-aid",
                 'nb'    => count($this->getDoctrine()->getRepository(Capacite::class)->findAll()),
             ),
             array(
                 'label' => "Compétences",
                 'slug'  => "competence",
-                'fa'    => "bookmark",
                 'nb'    => count($this->getDoctrine()->getRepository(Competence::class)->findAll()),
             ),
             array(
                 'label' => "Créatures",
                 'slug'  => "creature",
-                'fa'    => "bug",
                 'nb'    => count($this->getDoctrine()->getRepository(Creature::class)->findAll()),
             ),
             array(
                 'label' => "Factions",
                 'slug'  => "faction",
-                'fa'    => "address-book",
                 'nb'    => count($this->getDoctrine()->getRepository(Faction::class)->findAll()),
             ),
             array(
                 'label' => "Héros",
                 'slug'  => "hero",
-                'fa'    => "user-astronaut",
                 'nb'    => count($this->getDoctrine()->getRepository(Hero::class)->findAll()),
             ),
             array(
                 'label' => "Machines",
                 'slug'  => "machine_de_guerre",
-                'fa'    => "plus-square",
+                'nb'    => count($this->getDoctrine()->getRepository(MachineDeGuerre::class)->findAll()),
+            ),
+            array(
+                'label' => "Magies",
+                'slug'  => "magie",
+                'nb'    => count($this->getDoctrine()->getRepository(Magie::class)->findAll()),
+            ),
+            array(
+                'label' => "Panoplies",
+                'slug'  => "panoplie",
+                'nb'    => count($this->getDoctrine()->getRepository(Panoplie::class)->findAll()),
+            ),
+            array(
+                'label' => "Pelotons",
+                'slug'  => "peloton",
+                'nb'    => count($this->getDoctrine()->getRepository(Peloton::class)->findAll()),
+            ),
+            array(
+                'label' => "Ressources",
+                'slug'  => "ressource",
+                'nb'    => count($this->getDoctrine()->getRepository(Ressource::class)->findAll()),
+            ),
+            array(
+                'label' => "Semaines",
+                'slug'  => "semaine",
+                'nb'    => count($this->getDoctrine()->getRepository(Semaine::class)->findAll()),
+            ),
+            array(
+                'label' => "Sorts",
+                'slug'  => "sort",
+                'nb'    => count($this->getDoctrine()->getRepository(Sort::class)->findAll()),
+            ),
+            array(
+                'label' => "Spécialisations",
+                'slug'  => "specialisation",
+                'nb'    => count($this->getDoctrine()->getRepository(Specialisation::class)->findAll()),
+            ),
+            array(
+                'label' => "Terrains",
+                'slug'  => "terrain",
+                'nb'    => count($this->getDoctrine()->getRepository(Terrain::class)->findAll()),
+            ),
+            array(
+                'label' => "Villes",
+                'slug'  => "ville",
+                'nb'    => count($this->getDoctrine()->getRepository(Ville::class)->findAll()),
+            ),
+        );
+        return $this->render('dashboard/dashboard.html.twig', array(
+            "quickLinks" => $quickLinks
+        ));
+    }
+    
+    /**
+     * @Route("/get_nav_link", name="get_nav_link")
+     */
+    public function getNavLink()
+    {
+        $navLinks = array(
+            array(
+                'label' => "Artéfacts",
+                'slug'  => "artefact",
+                'nb'    => count($this->getDoctrine()->getRepository(Artefact::class)->findAll()),
+            ),
+            array(
+                'label' => "Batîments",
+                'slug'  => "batiment",
+                'nb'    => count($this->getDoctrine()->getRepository(Batiment::class)->findAll()),
+            ),
+            array(
+                'label' => "Capacités",
+                'slug'  => "capacite",
+                'nb'    => count($this->getDoctrine()->getRepository(Capacite::class)->findAll()),
+            ),
+            array(
+                'label' => "Compétences",
+                'slug'  => "competence",
+                'nb'    => count($this->getDoctrine()->getRepository(Competence::class)->findAll()),
+            ),
+            array(
+                'label' => "Créatures",
+                'slug'  => "creature",
+                'nb'    => count($this->getDoctrine()->getRepository(Creature::class)->findAll()),
+            ),
+            array(
+                'label' => "Factions",
+                'slug'  => "faction",
+                'nb'    => count($this->getDoctrine()->getRepository(Faction::class)->findAll()),
+            ),
+            array(
+                'label' => "Héros",
+                'slug'  => "hero",
+                'nb'    => count($this->getDoctrine()->getRepository(Hero::class)->findAll()),
+            ),
+            array(
+                'label' => "Machines",
+                'slug'  => "machine_de_guerre",
                 'nb'    => count($this->getDoctrine()->getRepository(MachineDeGuerre::class)->findAll()),
             ),
             array(
@@ -90,54 +179,46 @@ class DashboardController extends Controller
             array(
                 'label' => "Panoplies",
                 'slug'  => "panoplie",
-                'fa'    => "boxes",
                 'nb'    => count($this->getDoctrine()->getRepository(Panoplie::class)->findAll()),
             ),
             array(
                 'label' => "Pelotons",
                 'slug'  => "peloton",
-                'fa'    => "users",
                 'nb'    => count($this->getDoctrine()->getRepository(Peloton::class)->findAll()),
             ),
             array(
                 'label' => "Ressources",
                 'slug'  => "ressource",
-                'fa'    => "gem",
                 'nb'    => count($this->getDoctrine()->getRepository(Ressource::class)->findAll()),
             ),
             array(
                 'label' => "Semaines",
                 'slug'  => "semaine",
-                'fa'    => "calendar-alt",
                 'nb'    => count($this->getDoctrine()->getRepository(Semaine::class)->findAll()),
             ),
             array(
                 'label' => "Sorts",
                 'slug'  => "sort",
-                'fa'    => "star",
                 'nb'    => count($this->getDoctrine()->getRepository(Sort::class)->findAll()),
             ),
             array(
                 'label' => "Spécialisations",
                 'slug'  => "specialisation",
-                'fa'    => "tachometer-alt",
                 'nb'    => count($this->getDoctrine()->getRepository(Specialisation::class)->findAll()),
             ),
             array(
                 'label' => "Terrains",
                 'slug'  => "terrain",
-                'fa'    => "seedling",
                 'nb'    => count($this->getDoctrine()->getRepository(Terrain::class)->findAll()),
             ),
             array(
                 'label' => "Villes",
                 'slug'  => "ville",
-                'fa'    => "building",
                 'nb'    => count($this->getDoctrine()->getRepository(Ville::class)->findAll()),
             ),
         );
-        return $this->render('dashboard/dashboard.html.twig', array(
-            "quickLinks" => $quickLinks
+        return $this->render('dashboard/navLink.html.twig', array(
+            "navLinks" => $navLinks
         ));
     }
 }
